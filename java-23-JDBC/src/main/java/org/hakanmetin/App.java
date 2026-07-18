c6b2020e2f138e6d0697de4d01e2288d2ac7e1e5
Hakan Metin <hakanmetinxyz@outlook.de>
2026-07-18 14:54:28 +0200

    JDBC
    with PostgreSQL

--- java-23-JDBC/src/main/java/org/hakanmetin/App.java
+++
@@ -1,9 +0,0 @@
-package org.hakanmetin;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ java-23-JDBC/src/main/java/org/hakanmetin/MyPostgreSQLConnection.java
@@ -0,0 +1,61 @@
+package org.hakanmetin;
+
+import java.sql.*;
+
+public class MyPostgreSQLConnection {
+
+    public static void main(String[] args) {
+
+        Connection conn = null; //DB ile Baglanti kurmak icin
+        Statement statement = null; // Statementlar SQL cümleleri
+        ResultSet resultSet = null; //SQL Sorgularinin Sonuclari
+
+        try{  //Veri tabani olmayabilir, o yüzden TRY-CATCH
+            //Baglanti icin
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                                               "postgres",
+                                               "HAkan.01"); //Hangi veri tabanina gideceksek
+
+            statement = conn.createStatement();  //bu baglanti üzerinden sorgu olusturabiliriz
+
+            String sql1 = "SELECT * FROM my_company_db.customers" ;
+
+            //PostgreSQL wandelt Spaltennamen ohne Anführungszeichen automatisch in Kleinbuchstaben um.
+            String sql2 = "SELECT * FROM my_company_db.customers" + " WHERE \"AGE\"  > 25";
+
+            //gelen sonuclari buraya kaydediyorum
+            resultSet = statement.executeQuery(sql2);
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("FIRST_NAME")
+                + " " + resultSet.getString("LAST_NAME") ); //Kolonlar 1. // getString(3)); Indextende alabiliriz
+                System.out.println(resultSet.getInt("AGE"));
+                System.out.println(resultSet.getString("COUNTRY") + " " +
+                                   resultSet.getString("EMAIL") + " " +
+                                   resultSet.getString("GENDER") );
+
+
+                 // System.out.println(resultSet.getString("EMAIL"));
+                 // System.out.println(resultSet.getString("GENDER"));
+
+                System.out.println("-----");
+            }
+
+
+        }catch(SQLException e){
+            System.out.println("Fehler" + e);
+        } finally {
+            if (conn != null || statement != null || resultSet != null) {
+                try {
+                    resultSet.close();
+                    statement.close();
+                    conn.close();
+
+                }catch (SQLException e) {
+                    throw new RuntimeException();
+                }
+            }
+        }
+    }
+
+}
