6fd0d1ff38ac79637d8e3d2cf7db5797a97fbb21
Hakan Metin <hakanmetinxyz@outlook.de>
2026-07-19 14:52:49 +0200

    CRUD
    mit JDBC wurde das CRUD Konzept Idee sehr basic umgesetzt

--- java-23-JDBC/src/main/java/org/hakanmetin/_02_crud/AppMain.java
+++ java-23-JDBC/src/main/java/org/hakanmetin/_02_crud/AppMain.java
@@ -46,3 +46,121 @@ public class AppMain {
 
     }
 }
+
+/*
+*NOTIZEN für CRUD Thema 
+*
+*
+*Warum braucht man CRUD?
+Angenommen, du entwickelst eine Kundenverwaltung. Dann muss die Anwendung typischerweise:
+einen Kunden anlegen,
+einen Kunden anzeigen,
+Kundendaten ändern,
+einen Kunden löschen.
+Das sind praktisch alle grundlegenden Aktionen, die man mit einem Datensatz ausführt.
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
+
+
+/*
+*
+*
+*
+*
+* Für das Verständnis ist der Code in Ordnung.
+* In einer echten Anwendung sollte aber nicht die gesamte SQL-Anweisung an die Methode übergeben werden:
+*
+* z.B.
+* myCrud.deleteCustomerById(105);
+*
+*
+*
+* public void deleteCustomerById(int customerId) {
+    String sql = """
+        DELETE FROM my_company_db.customers
+        WHERE "CUSTOMER_ID" = ?
+        """;
+
+    try (
+        Connection connection =
+                DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+        PreparedStatement statement =
+                connection.prepareStatement(sql)
+    ) {
+        statement.setInt(1, customerId);
+
+        int affectedRows = statement.executeUpdate();
+
+        System.out.println(affectedRows + " Kunde(n) gelöscht.");
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
+*
+* */
+
+
+/*
+*
+*
+* Das ist aus mehreren Gründen besser:
+Der Aufrufer muss kein SQL kennen.
+Die Methode kann wiederverwendet werden.
+Werte werden sicher über ? eingesetzt.
+Verbindung und Statement werden automatisch geschlossen.
+*
+*
+* */
+
+/*Der wichtigste Vorteil von PreparedStatement entsteht erst durch Platzhalter:
+*
+*String sql = """
+    DELETE FROM my_company_db.customers
+    WHERE "CUSTOMER_ID" = ?
+    """;
+* */
+
+
+/*
+*
+* Der CRUD-Gedanke bleibt gleich. Nur die technische Umsetzung ändert sich.
+Mit JDBC schreibe ich SQL selbst:
+*
+* String sql = "DELETE FROM customers WHERE id = ?";
+statement.setInt(1, id);
+statement.executeUpdate();
+*
+* Mit Hibernate arbeite ich stärker mit Java-Objekten:
+*
+*Customer customer = entityManager.find(Customer.class, 105);
+entityManager.remove(customer);
+*
+* Hibernate erzeugt daraus ungefähr:
+*
+* DELETE FROM customers WHERE customer_id = 105;
+*
+* CRUD gehört also nicht speziell zu Hibernate. Es ist ein allgemeines Konzept. JDBC, Hibernate,
+* Spring Data JPA und andere Technologien bieten lediglich unterschiedliche Wege, CRUD umzusetzen.
+*
+*
+*
+*
+* Der zentrale Gedanke lautet:
+CRUD definiert die grundlegenden Aktionen, die meine Anwendung mit dauerhaft gespeicherten Objekten
+* oder Datensätzen ausführen kann.
+* */
+
+
+
+
