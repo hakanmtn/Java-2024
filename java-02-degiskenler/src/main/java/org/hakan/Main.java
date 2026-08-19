f5c0c7381f58b357355fbf5fdeab8bdcaf985381
Hakan Metin <hakanmetinxyz@outlook.de>
2026-08-19 20:29:46 +0200

    Update Main.java

--- java-02-degiskenler/src/main/java/org/hakan/Main.java
+++ java-02-degiskenler/src/main/java/org/hakan/Main.java
@@ -3,5 +3,17 @@ package org.hakan;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+
+
+
+        Integer age = 29;
+        changeAge(age);
+        System.out.println(age); // weiterhin 29
+    }
+
+    private static void changeAge(Integer age) {
+        age = 99;
     }
 }
